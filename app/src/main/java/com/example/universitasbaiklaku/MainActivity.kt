5b6d898d4216750db8f8583437cf75dfd1395fec
Your Name <you@example.com>
2022-10-29 04:23:18 +0700

    RecycleView

---
+++ app/src/main/java/com/example/universitasbaiklaku/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.universitasbaiklaku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var AdprProdi : AdapterProdi
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+
+
+    }
+
---
+++ app/src/main/java/com/example/universitasbaiklaku/Prodi.kt
@@ -0,0 +1,9 @@
+package com.example.universitasbaiklaku
+
+class Prodi(
+    val warna       : String,
+    val jenjang     : String,
+    val nmProdi     : String,
+    val fakultas    : String,
+    val konsentrasi : Array<String> = arrayOf(nmProdi)
+)
